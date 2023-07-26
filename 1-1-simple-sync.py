4088c81380ea4aa7dd5e241851104e5cd589a109
Khembhicha <6310301032@cdti.ac.th>
2023-07-26 14:12:42 +0700

    commit

--- 1-1-simple-sync.py
+++ 1-1-simple-sync.py
@@ -1,14 +1,12 @@
-# 1-1-simple-sync.py
+# import time
 import time
 
-
+# suspend execution of the calling thread for however many seconds.
 def sleep():
     print(f'Time: {time.time() - start:.2f}')
     time.sleep(1)
 
-#
-
-
+# plus number
 def sum(name, numbers):
     total = 0
     for number in numbers:
@@ -18,13 +16,13 @@ def sum(name, numbers):
     #
     print(f'Task {name}: Sum = {total}\n ')
 
-
+# measure the total time elapsed to execute the script in seconds at Start.
 start = time.time()
-#
+# work value
 tasks = [
     sum("A", [1, 2]),
     sum("B", [1, 2, 3])
 ]
-
+# measure the total time elapsed to execute the script in seconds at End.
 end = time.time()
 print(f'Time: {end-start:.2f} sec')
